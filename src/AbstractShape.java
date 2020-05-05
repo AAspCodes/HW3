8c90c27fa81c91603b7a763731a5a02b0f8595bd
AAspCodes <asp.anthony1@gmail.com>
2020-05-05 12:16:27 -0700

    create AbstractShape method stubs

---
+++ src/AbstractShape.java
@@ -0,0 +1,43 @@
+import java.awt.Graphics;
+
+/**
+ * 
+ */
+
+/**
+ * @author  Anthony Asp & Michael Cho
+ *
+ */
+public abstract class AbstractShape implements Shape {
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean addLevel() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeLevel() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int countShapes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void update(int value) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
