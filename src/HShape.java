516311c855a4ee75e1ad721b1d52cddd2dfbaa69
AAspCodes <asp.anthony1@gmail.com>
2020-05-05 12:33:18 -0700

    add empty javadoc comments, and remove uneeded ones

--- src/HShape.java
+++ src/HShape.java
@@ -1,13 +1,12 @@
-/**
- * 
- */
 
 /**
  * @author  Anthony Asp & Michael Cho
  *
  */
 public class HShape extends AbstractShape {
-
+	/**
+	 * 
+	 */
 	@Override
 	public void createChildren() {
 		// TODO Auto-generated method stub
--- src/MyShape.java
+++ src/MyShape.java
@@ -1,17 +1,16 @@
-/**
- * 
- */
 
 /**
- * @author  Anthony Asp & Michael Cho
+ * @author Anthony Asp & Michael Cho
  *
  */
 public class MyShape extends AbstractShape {
-
+	/**
+	 * 
+	 */
 	@Override
 	public void createChildren() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
