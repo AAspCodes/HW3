ecc33f0dfac3b02388da38509d9743ecf632d8da
AAspCodes <asp.anthony1@gmail.com>
2020-05-05 12:30:40 -0700

    add empty java doc comments to AbstractShape methods

--- src/AbstractShape.java
+++ src/AbstractShape.java
@@ -5,39 +5,60 @@ import java.awt.Graphics;
  */
 
 /**
- * @author  Anthony Asp & Michael Cho
+ * @author Anthony Asp & Michael Cho
  *
  */
 public abstract class AbstractShape implements Shape {
+	protected AbstractShape[] childShapes;
 
+	/**
+	 * @param Graphics g: java.awt.Graphics
+	 */
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public boolean addLevel() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public boolean removeLevel() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public int countShapes() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void update(int value) {
 		// TODO Auto-generated method stub
 
 	}
+
+	/**
+	 * 
+	 */
+	abstract public void createChildren();
 
 }
