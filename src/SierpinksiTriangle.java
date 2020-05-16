b2512de4a86cef676b2943ffcb9247f452a80777
mychaelcho <mychael.cho@gmail.com>
2020-05-15 23:41:01 -0700

    Attempt at SierpinksiTriangle
    Can't seem to get it to work. Here's what I have so far.

--- src/SierpinksiTriangle.java
+++ src/SierpinksiTriangle.java
@@ -1,30 +1,56 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Polygon;
 
 /**
  * @author Anthony Asp & Michael Cho
  *
  */
 public class SierpinksiTriangle extends AbstractShape {
+	private int width, height, drawStartX, drawStartY;
+
+	public SierpinksiTriangle(int WIDTH, int HEIGHT) {
+		super(10, 1, Color.GREEN);
+		this.width = WIDTH;
+		this.height = HEIGHT;
+		createChildren();
+	}
+
+	public SierpinksiTriangle(int drawStartX, int drawStartY, int width, int height, int level) {
+		super(10, level, Color.GREEN);
+		this.drawStartX = drawStartX;
+		this.drawStartY = drawStartY;
+		this.width = width;
+		this.height = height;
+	}
 
 	/**
 	 * 
 	 */
-	protected SierpinksiTriangle() {
-		super(3, 1, Color.GREEN);
-
-	}
-
 	@Override
 	public void createChildren() {
-		// TODO Auto-generated method stub
-
+		this.children = new AbstractShape[maxLevel];
+		
+		children[0] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[1] = new SierpinksiTriangle(drawStartX + (width / 4), drawStartY, width / 2, height / 2, level + 1);
+		children[2] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[3] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[4] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[5] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[6] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[7] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[8] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
+		children[9] = new SierpinksiTriangle(drawStartX, drawStartY, width, height, level + 1);
 	}
+
 
 	@Override
 	public void drawBaseShape(Graphics g) {
-		// TODO Auto-generated method stub
+		g.setColor(color);
+		
+		int[] xPoints = {drawStartX + ((width/4) * 2), drawStartX, drawStartX + width, drawStartX + ((width/4) * 2)};
+		int[] yPoints = {drawStartY, drawStartY + height, drawStartY + height, drawStartY};
+			
+		g.drawPolyline(xPoints, yPoints, 4);
 
 	}
 
